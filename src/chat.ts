78bb55fa935b15ad1b65368b55cf4c32c03cb44a
sujang958 <enter7377@naver.com>
2023-01-01 20:03:04 +0900

    feat: add storing

--- src/chat.ts
+++ src/chat.ts
@@ -2,22 +2,32 @@
 
 import tmi from "tmi.js"
 
-const client = new tmi.client({
-  identity: {
-    username: "Charla",
-    password: "jrwb2k0tikjgq2u4izmk0wrot08zjo",
-  },
-  channels: [
-    "omom1972"
-  ]
-})
+export const getClient = ({
+  password,
+  channel,
+}: {
+  password: string
+  channel: string
+}) => {
+  const client = new tmi.client({
+    identity: {
+      username: "Charla",
+      password: password,
+    },
+    channels: [channel],
+  })
 
-client.on('connected', (address, port) => {
-  console.log("Connected to Twitch", address, port)
-})
+  client.on("connected", (address, port) => {
+    console.log("Connected to Twitch", address, port)
+  })
 
-client.on("message", (channel, userState, message) => {
-  console.log(channel, userState, message)
-})
+  return client
+}
 
-client.connect()
+
+
+// client.on("message", (channel, userState, message) => {
+//   console.log(channel, userState, message)
+// })
+
+// client.connect()
