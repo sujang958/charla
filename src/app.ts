d4d28e0d369e6b2790b2d8bfde0d2f888551b838
sujang958 <enter7377@naver.com>
2023-01-04 21:34:39 +0900

    feat: add Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,47 @@
-import { app, BrowserWindow } from "electron"
+import { app, BrowserWindow, ipcMain } from "electron"
 
 const createChatWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 400,
-    frame: false,
+    frame: true,
     transparent: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   })
+
+  ipcMain.handle(
+    "sign-in",
+    () =>
+      new Promise((resolve, reject) => {
+        const OauthWin = new BrowserWindow({
+          width: 600,
+          height: 500,
+        })
+
+        OauthWin.loadURL(
+          "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=pc6ojm41czbrk6mkmt60cqotlb4qqt&scope=chat:read&redirect_uri=http%3A%2F%2Flocalhost%3A4000"
+        )
+
+        OauthWin.webContents.on("will-navigate", (_, url) => {
+          const newUrl = new URL(url)
+          const accessToken = newUrl.hash
+            .split("&")[0]
+            .replace("#access_token=", "")
+
+          OauthWin.close()
+
+          resolve(accessToken)
+        })
+      })
+  )
 
   win.loadFile("screens/index.html")
 }
 
-const createSettingWindow = () => {
-  const win = new BrowserWindow({
-    width: 300,
-    height: 220,
-    resizable: false,
-  })
-
-  win.loadFile("screens/setting.html")
-}
-
 app.whenReady().then(() => {
   createChatWindow()
-  createSettingWindow()
+  // createSettingWindow()
 })
