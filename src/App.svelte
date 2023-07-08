c06e40082055db79a944e881a28060fc1ca960f9
sujang.958 <enter7377@naver.com>
2023-07-08 19:47:22 +0900

    feat: implement scrolling to the bottom

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import tmi from "tmi.js"
   import Preferences from "./lib/Preferences.svelte"
-  import {color} from "./stores/color"
-  
+  import { color } from "./stores/color"
 
   const client = new tmi.Client({
     options: { debug: true },
@@ -13,6 +12,7 @@
   })
 
   let messages: { message: string; size: number }[] = []
+  let messageContainer: HTMLDivElement
 
   client.connect().catch(console.error)
 
@@ -26,16 +26,20 @@
     console.log(formattedMessage)
 
     messages = [
-      ...messages,
+      ...(messages.length > 100 ? [] : messages),
       {
         message: formattedMessage,
         size: Number(localStorage.getItem("fontSize") ?? "1.5"),
       },
     ]
+    messageContainer.scrollTo(0, document.body.scrollHeight)
   })
 </script>
 
-<main class={`w-full h-screen text-white relative`} style={`background-color: ${$color};`}>
+<main
+  class={`w-full min-h-screen text-white relative`}
+  style={`background-color: ${$color};`}
+>
   <div class="fixed bottom-4 right-4">
     <svg
       data-tauri-drag-region
@@ -52,7 +56,7 @@
       />
     </svg>
   </div>
-  <div class="flex flex-col gap-y-0.5 p-2 w-full">
+  <div class="flex flex-col gap-y-0.5 p-2 w-full" bind:this={messageContainer}>
     {#each messages as { message, size }}
       <p class={`text-[${size}rem] font-bold`}>{message}</p>
     {/each}
